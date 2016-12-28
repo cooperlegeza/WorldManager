6e6ac20095add9ca01aedc3114f77b4e60b599ee
Cooper Legeza <cooperlegeza@gmail.com>
2016-12-28 15:25:56 -0500

    Added test functionality to campaign service spec

--- frontend/src/world-manager-app.js
+++ frontend/src/world-manager-app.js
@@ -2,12 +2,12 @@ import angular from 'angular'
 import uiRouter from 'angular-ui-router';
 import worldsController from './worlds/worldsController';
 import routesConfig from './router-config.js';
-import campaignService from './campaigns/campaignDataFactoryService';
+import CampaignService from './campaigns/campaignDataFactoryService';
 
 const worldManagerApp = angular.module('worldManagerApp', [
     uiRouter,
 ]).controller('worldsController', worldsController)
-    .factory(campaignService)
+    .factory('CampaignService', CampaignService)
     .config(routesConfig);
 
 export default worldManagerApp.name;
--- frontend/test/campaigns/campaignDataFactoryService.spec.js
+++ frontend/test/campaigns/campaignDataFactoryService.spec.js
@@ -1,1 +1,27 @@
 import angular from 'angular';
+import 'angular-mocks';
+import worldManagerApp from '../../src/world-manager-app';
+
+const {inject, module} = angular.mock;
+
+describe('CampaignService', ()=> {
+   let $httpBackend;
+   let successHandler;
+   let errorHandler;
+   let CampaignService;
+
+   beforeEach(module(worldManagerApp));
+
+   beforeEach(inject((_$httpBackend_, _CampaignService_) => {
+       $httpBackend = _$httpBackend_;
+       CampaignService = _CampaignService_;
+
+       successHandler = jasmine.createSpy('successHandler');
+       errorHandler = jasmine.createSpy('errorHandler');
+   }));
+
+   it('should exist', () => {
+      expect(CampaignService).toBeDefined();
+   });
+
+});
